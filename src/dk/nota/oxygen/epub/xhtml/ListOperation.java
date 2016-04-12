afb79abe28d21c398f80328a124d6510b7c17b29
stesk <ssp@nota.nu>
2016-04-12 16:17:05 +0200

    Update list operation

--- src/dk/nota/oxygen/epub/xhtml/ListOperation.java
+++ src/dk/nota/oxygen/epub/xhtml/ListOperation.java
@@ -12,32 +12,50 @@ import ro.sync.ecss.extensions.api.node.AuthorNode;
 
 public class ListOperation extends XhtmlEpubAuthorOperation {
 	
-	private ListType listType;
+	private OperationType operationType;
+	
+	private void convert(LinkedList<AuthorElement> blocks)
+			throws AuthorOperationException {
+		int start = blocks.getFirst().getStartOffset();
+		int end = blocks.getLast().getEndOffset();
+		for (AuthorElement block : blocks) {
+			if (block.getName().equals("p"))
+				getDocumentController().renameElement(block, "li");
+			else if (block.getName().matches("^(ol|ul)$"))
+				wrapInFragment(operationType.getListItemFragment(),
+						block.getStartOffset(), block.getEndOffset());
+		}
+		wrapInFragment(operationType.getListFragment(), start, end);
+	}
+	
+	private void dissolve(LinkedList<AuthorElement> listItems)
+			throws AuthorOperationException, BadLocationException {
+		int start = listItems.getFirst().getStartOffset();
+		int end = listItems.getLast().getEndOffset();
+		floatInterval(start, end);
+	}
 	
 	@Override
 	protected void doOperation() throws AuthorOperationException {
 		try {
 			LinkedList<AuthorElement> blocks = new LinkedList<AuthorElement>();
-			// Get relevant selected blocks: list items or paragraphs
 			for (AuthorNode node : getSelectedNodes()) {
 				AuthorElement block = getFirstElementByXpath(
-						"ancestor-or-self::li|ancestor-or-self::p", node);
+						operationType.isConversion() ? "(ancestor-or-self::ol|" +
+						"ancestor-or-self::p|ancestor-or-self::ul)[1]" :
+						"ancestor-or-self::li[1]", node);
 				if (block != null && !blocks.contains(block)) blocks.add(block);
 			}
-			for (AuthorElement block : blocks)
-				getDocumentController().renameElement(block,
-						listType == ListType.NONE ? "p" : "li");
-			AuthorElement parent = (AuthorElement)getCommonParent(blocks
-					.getFirst().getStartOffset(), blocks.getLast()
-					.getEndOffset());
-			if (parent.getLocalName().matches("^(ol|ul)$")) {
-				if (listType == ListType.NONE) floatInterval(blocks.getFirst()
-						.getStartOffset(), blocks.getLast().getEndOffset());
-				else getDocumentController().renameElement(parent,
-						listType.getName());
-			} else {
-				wrapInFragment(listType.getFragment(), blocks.getFirst()
-						.getStartOffset(), blocks.getLast().getEndOffset());
+			switch (operationType) {
+			case CONVERT_OL: case CONVERT_UL:
+				convert(blocks);
+				return;
+			case INDENT_OL: case INDENT_UL:
+				indent(blocks);
+				return;
+			case DISSOLVE:
+				dissolve(blocks);
+				return;
 			}
 		} catch (BadLocationException e) {
 			throw new AuthorOperationException(e.toString());
@@ -47,10 +65,12 @@ public class ListOperation extends XhtmlEpubAuthorOperation {
 	@Override
 	public ArgumentDescriptor[] getArguments() {
 		return new ArgumentDescriptor[] {
-				new ArgumentDescriptor("listType", ArgumentDescriptor
-						.TYPE_CONSTANT_LIST, "The desired list type",
-						new String[] {"ordered", "unordered", "none"},
-						"unordered")
+				new ArgumentDescriptor("operationType", ArgumentDescriptor
+						.TYPE_CONSTANT_LIST, "The desired operation type",
+						new String[] {"convertOrdered", "convertUnordered",
+								"indentOrdered", "indentUnordered",
+								"dissolve"},
+						"convertOrdered")
 		};
 	}
 
@@ -58,32 +78,65 @@ public class ListOperation extends XhtmlEpubAuthorOperation {
 	public String getDescription() {
 		return "Allows manipulation of lists and list items";
 	}
+	
+	private void indent(LinkedList<AuthorElement> blocks)
+			throws AuthorOperationException {
+		int start = blocks.getFirst().getStartOffset();
+		int end = blocks.getLast().getEndOffset();
+		wrapInFragment(operationType.getListFragment(), start, end);
+		wrapInFragment(operationType.getListItemFragment(), start, end);
+	}
 
 	@Override
 	protected void parseArguments(ArgumentsMap arguments)
 			throws IllegalArgumentException {
-		String argument = (String)arguments.getArgumentValue("listType");
-		if (argument.equals("ordered")) listType = ListType.ORDERED;
-		else if (argument.equals("unordered")) listType = ListType.UNORDERED;
-		else if (argument.equals("none")) listType = ListType.NONE;
+		String typeString = (String)arguments.getArgumentValue("operationType");
+		switch (typeString) {
+		case "convertOrdered":
+			operationType = OperationType.CONVERT_OL;
+			break;
+		case "convertUnordered":
+			operationType = OperationType.CONVERT_UL;
+			break;
+		case "indentOrdered":
+			operationType = OperationType.INDENT_OL;
+			break;
+		case "indentUnordered":
+			operationType = OperationType.INDENT_UL;
+			break;
+		case "dissolve":
+			operationType = OperationType.DISSOLVE;
+			break;
+		}
 	}
 	
-	public enum ListType {
+	public enum OperationType {
 		
-		ORDERED, UNORDERED, NONE;
+		CONVERT_OL, CONVERT_UL, INDENT_OL, INDENT_UL, DISSOLVE;
 		
-		public String getFragment() {
+		public boolean isConversion() {
+			return (this == CONVERT_OL || this == CONVERT_UL);
+		}
+		
+		public String getListFragment() {
 			switch (this) {
-			case ORDERED: return "<ol xmlns='http://www.w3.org/1999/xhtml'/>";
-			case UNORDERED: return "<ul xmlns='http://www.w3.org/1999/xhtml'/>";
-			default: return null;
+			case CONVERT_OL: case INDENT_OL:
+				return "<ol xmlns='http://www.w3.org/1999/xhtml'/>";
+			case CONVERT_UL: case INDENT_UL:
+				return "<ul xmlns='http://www.w3.org/1999/xhtml'/>";
+			default:
+				return null;
 			}
+		}
+		
+		public String getListItemFragment() {
+			return "<li xmlns='http://www.w3.org/1999/xhtml'/>";
 		}
 		
 		public String getName() {
 			switch (this) {
-			case ORDERED: return "ol";
-			case UNORDERED: return "ul";
+			case CONVERT_OL: case INDENT_OL: return "ol";
+			case CONVERT_UL: case INDENT_UL: return "ul";
 			default: return null;
 			}
 		}
