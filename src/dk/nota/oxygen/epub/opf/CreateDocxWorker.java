38855e9b9462414521dbce83934251fc9be21658
stesk <ssp@nota.nu>
2016-08-17 16:10:09 +0200

    Perform DTBook-Docx conversion

--- src/dk/nota/oxygen/epub/opf/CreateDocxWorker.java
+++ src/dk/nota/oxygen/epub/opf/CreateDocxWorker.java
@@ -1,28 +1,42 @@
 package dk.nota.oxygen.epub.opf;
 
-import de.schlichtherle.io.DefaultArchiveDetector;
-import de.schlichtherle.io.File;
+import java.net.URI;
+
 import dk.nota.oxygen.common.ConsoleWindow;
 import dk.nota.oxygen.common.EditorAccess;
-import dk.nota.oxygen.common.ZipArchiveDetector;
 import dk.nota.oxygen.epub.common.EpubAccess;
+import net.sf.saxon.s9api.XdmDestination;
 import net.sf.saxon.s9api.XdmNode;
+import net.sf.saxon.s9api.XsltTransformer;
 
 public class CreateDocxWorker extends CreateDtbWorker {
 	
-	private File docxFile;
+	private URI docxFileUri;
 	
 	public CreateDocxWorker(EditorAccess editorAccess, EpubAccess epubAccess,
 			ConsoleWindow consoleWindow, java.io.File docxFile) {
 		super(editorAccess, epubAccess, consoleWindow, docxFile, true, false);
-		this.docxFile = new ZipArchiveDetector().createFile(docxFile);
+		docxFileUri = docxFile.toURI().resolve(docxFile.getName() + "/");
 	}
 
 	@Override
 	protected Object doInBackground() throws Exception {
 		XdmNode dtbDocument = (XdmNode)super.doInBackground();
 		getConsoleWindow().writeToConsole("CONVERTING TO DOCX...");
+		XsltTransformer docxTransformer = getEpubAccess().getXmlAccess()
+				.getXsltTransformer("dtbook2docx/dtbook2docx.xsl");
+		docxTransformer.setInitialContextNode(dtbDocument);
+		docxTransformer.setDestination(new XdmDestination());
+		docxTransformer.setBaseOutputURI(docxFileUri.toString());
+		docxTransformer.transform();
+		copyImages(docxFileUri.resolve("word/media/"));
+		
 		return null;
+	}
+	
+	@Override
+	protected void done() {
+		getConsoleWindow().writeToConsole("DOCX CONVERSION DONE");
 	}
 
 }
