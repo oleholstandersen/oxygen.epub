d6c101ed982ed234f15e6e2bd9196bd443f014f2
stesk <ssp@nota.nu>
2017-02-09 11:10:15 +0100

    Indent try block

--- src/dk/nota/oxygen/epub/opf/CreateDocxAction.java
+++ src/dk/nota/oxygen/epub/opf/CreateDocxAction.java
@@ -18,16 +18,17 @@ public class CreateDocxAction extends ArchiveSensitiveAction {
 	@Override
 	public void actionPerformed(EditorAccess editorAccess) {
 		try {
-		EpubAccess epubAccess = editorAccess.getEpubAccess();
-		String docxFileName = "./" + epubAccess.getPid() + ".docx";
-		File docxFile = editorAccess.getWorkspace().chooseFile(new File(
-				epubAccess.getArchiveFileUrl().toURI().resolve(docxFileName)),
-				"Export [Docx]", new String[] {"docx"}, "Word documents",
-				true);
-		if (docxFile == null) return;
-		CreateDocxWorker createDocxWorker = new CreateDocxWorker(editorAccess,
-				epubAccess, new ConsoleWindow("Create Docx"), docxFile);
-		createDocxWorker.execute();
+			EpubAccess epubAccess = editorAccess.getEpubAccess();
+			String docxFileName = "./" + epubAccess.getPid() + ".docx";
+			File docxFile = editorAccess.getWorkspace().chooseFile(new File(
+					epubAccess.getArchiveFileUrl().toURI().resolve(
+					docxFileName)), "Export [Docx]", new String[] {"docx"},
+					"Word documents", true);
+			if (docxFile == null) return;
+			CreateDocxWorker createDocxWorker = new CreateDocxWorker(
+					editorAccess, epubAccess, new ConsoleWindow("Create Docx"),
+					docxFile);
+			createDocxWorker.execute();
 		} catch (SaxonApiException | URISyntaxException e) {
 			editorAccess.showErrorMessage(e.toString());
 		}
