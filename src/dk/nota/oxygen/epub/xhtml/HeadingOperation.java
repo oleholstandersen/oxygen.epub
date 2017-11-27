ee9c7974f75acadef3448eeedf3bd588b7dfd022
stesk <ssp@nota.nu>
2017-11-27 10:18:02 +0100

    Improve manipulation of headings in XHTML

--- src/dk/nota/oxygen/epub/xhtml/HeadingOperation.java
+++ src/dk/nota/oxygen/epub/xhtml/HeadingOperation.java
@@ -11,119 +11,112 @@ import ro.sync.ecss.extensions.api.node.AuthorNode;
 
 public class HeadingOperation extends XhtmlEpubAuthorOperation {
 	
+	private int depth;
 	private boolean dissolve;
+	private boolean editingConcatDocument;
 	private int newDepth;
 	private String sectionFragment =
 			"<section xmlns='http://www.w3.org/1999/xhtml'/>";
+	private boolean shift;
+	
+	private int determineSectionEnd(AuthorElement section)
+			throws AuthorOperationException {
+		// Basically decide whether to include following subsections or not
+		AuthorElement[] subsections = getElementsByXpath("section", section);
+		if (!shift && subsections.length > 0)
+			return subsections[0].getStartOffset();
+		else return section.getEndOffset();
+	}
 	
 	private int determineSectionStart(AuthorElement heading)
 			throws AuthorOperationException {
+		// If the heading candidate is preceded by a page number, the number
+		// must be included in the section we establish later
 		AuthorElement precedingBreak = getFirstElementByXpath(
 				"preceding-sibling::*[1][@epub:type = 'pagebreak']", heading);
 		if (precedingBreak != null) return precedingBreak.getStartOffset();
 		return heading.getStartOffset();
 	}
-	
-	private void dissolveSection(AuthorElement section, int depth)
-			throws AuthorOperationException, BadLocationException {
-		if (section.getName().equals("body")) return;
-		AuthorElement precedingSection = getFirstElementByXpath(
-				"preceding-sibling::*[1]/self::section", section);
-		normaliseToDepth(section, --depth);
-		if (precedingSection == null) dissolveElement(section);
-		else {
-			AuthorDocumentFragment sectionContent = getDocumentController().
-					createDocumentFragment(section.getStartOffset() + 1,
-							section.getEndOffset() - 1);
-			getDocumentController().deleteNode(section);
-			getDocumentController().insertFragment(precedingSection
-					.getEndOffset(), sectionContent);
-		}
-	}
 
 	@Override
 	protected void doOperation() throws AuthorOperationException {
+		// TODO: Make cursor position flexible using the Position class
+		int caretOffsetAtStart = getAuthorEditor().getCaretOffset();
+		// Get the heading candidate (operation is only available if such a
+		// candidate is present, so no need to check for null)
+		AuthorElement heading = getFirstElementByXpath("ancestor-or-self::p|"
+				+ "ancestor-or-self::*[matches(local-name(), 'h\\d')]");
 		try {
-			AuthorElement heading = getFirstElementByXpath(
-					"ancestor-or-self::*[parent::body or parent::section][1]");
-			if (dissolve) getDocumentController().renameElement(heading, "p");
-			else {
-				stripElements(heading, "em", "strong");
-				getDocumentController().renameElement(heading, "h" + newDepth);
-			}
+			// Get ancestor sections
 			AuthorElement[] ancestorSections = getElementsByXpath(
 					"ancestor::body|ancestor::section", heading);
-			int depth = ancestorSections.length;
+			// Get current depth (= number of ancestor sections), may be 0;
+			// note that depth is lower if we are editing a concat document
+			depth = editingConcatDocument ? ancestorSections.length - 1 :
+				ancestorSections.length;
 			AuthorElement parentSection = ancestorSections[depth - 1];
-			if (editingConcatDocument()) depth--; // Concat documents are deeper
+			int end = determineSectionEnd(parentSection);
 			if (dissolve) {
-				if (!parentSection.getName().equals("body")) dissolveSection(
-						parentSection, depth);
+				getDocumentController().renameElement(heading, "p");
+				if (shift) normaliseToDepth(parentSection, depth - 2);
+				else {
+					splitNodeAtOffset(parentSection, end, true);
+					parentSection = (AuthorElement)getDocumentController()
+							.getNodeAtOffset(end);
+				}
+				dissolveElement(parentSection);
 				return;
 			}
-			normaliseToDepth(getElementsByXpath("following-sibling::*",
-					heading), newDepth);
 			int start = determineSectionStart(heading);
-			if (newDepth == depth) {
-				if (!parentSection.getName().equals("body")) splitNodeAtOffset(
-						parentSection, start, true);
-				return;
-			}
-			AuthorElement section = establishSection(parentSection, start);
-			if (newDepth > depth) insertBelow(section, newDepth - depth);
-			else if (newDepth < depth) insertAbove(section, depth - newDepth);
+			getDocumentController().renameElement(heading, "h" + newDepth);
+			AuthorElement section = establishSection(parentSection, start, end);
+			if (shift) normaliseToDepth(section, newDepth - 1);
+			if (newDepth < depth)
+				// If the established section is the parent section, we need
+				// the actual parent section
+				insertSectionAbove(section, depth - newDepth);
+			else if (newDepth > depth)
+				insertSectionBelow(section, newDepth - depth);
 		} catch (BadLocationException e) {
 			throw new AuthorOperationException(e.toString());
+		} finally {
+			getAuthorEditor().setCaretPosition(caretOffsetAtStart);
 		}
 	}
 	
-	private AuthorElement establishSection(AuthorElement section, int start)
-			throws AuthorOperationException, BadLocationException {
-		if (!section.getName().equals("body")) {
-			if (section.getContentNodes().get(0).getStartOffset() == start)
-				return section;
+	private AuthorElement establishSection(AuthorElement parentSection,
+			int start, int end) throws AuthorOperationException,
+			BadLocationException {
+		// If new section starts at same offset as parent section 
+		if (start - parentSection.getStartOffset() <= 1) {
+			if (shift) return parentSection;
+			else {
+				splitNodeAtOffset(parentSection, end, true);
+				return (AuthorElement)getDocumentController().getNodeAtOffset(
+						end);
+			}
 		}
-		getDocumentController().surroundInFragment(sectionFragment, start++,
-				section.getEndOffset() - 1);
-		newDepth--;
+		// Establish a section
+		getDocumentController().surroundInFragment(sectionFragment, ++start,
+				--end);
+		// With the new section we increase the working depth
+		depth++;
 		return (AuthorElement)getDocumentController().getNodeAtOffset(start);
 	}
-
-	@Override
-	public ArgumentDescriptor[] getArguments() {
-		return new ArgumentDescriptor[] {
-				new ArgumentDescriptor("newDepth", ArgumentDescriptor
-						.TYPE_CONSTANT_LIST, "The desired new heading depth",
-						new String[] {"1", "2", "3", "4", "5", "6", "NONE"},
-						"NONE")
-		};
-	}
-
-	@Override
-	public String getDescription() {
-		return "Allows manipulation of heading and sectioning elements";
-	}
 	
-	private void insertAbove(AuthorNode section, int iterations)
+	private void insertSectionAbove(AuthorElement section, int iterations)
 			throws AuthorOperationException, BadLocationException {
 		if (section.getParent().getName().equals("body")) {
 			floatInterval(section.getStartOffset() + 1,
 					section.getEndOffset() - 1);
 			return;
 		}
-		section = floatNode(section);
-		if (iterations > 1) insertAbove(section, --iterations);
+		section = (AuthorElement)floatNode(section);
+		if (iterations > 1) insertSectionAbove(section, --iterations);
 	}
 	
-	private void insertBelow(AuthorNode section, int iterations)
+	private void insertSectionBelow(AuthorNode section, int iterations)
 			throws AuthorOperationException, BadLocationException {
-		AuthorElement precedingSection = getFirstElementByXpath(
-				"preceding-sibling::*[1]/self::section", section);
-		if (precedingSection != null) {
-			section = moveNodeToOffset(section, precedingSection
-					.getEndOffset());
-			iterations--;
-		}
 		int start = section.getStartOffset();
 		int end = section.getEndOffset();
 		for (int i = 1; i <= iterations; i++)
@@ -132,11 +125,32 @@ public class HeadingOperation extends XhtmlEpubAuthorOperation {
 	}
 
 	@Override
+	public ArgumentDescriptor[] getArguments() {
+		return new ArgumentDescriptor[] {
+				new ArgumentDescriptor("newDepth", ArgumentDescriptor
+						.TYPE_CONSTANT_LIST, "The desired new heading depth",
+						new String[] {"1", "2", "3", "4", "5", "6", "none"},
+						"none"),
+				new ArgumentDescriptor("shift", ArgumentDescriptor
+						.TYPE_CONSTANT_LIST, "Shift subsequent sections up/down",
+						new String[] {"true", "false"}, "false"),
+		};
+	}
+
+	@Override
+	public String getDescription() {
+		return "Allows manipulation of heading and sectioning elements";
+	}
+
+	@Override
 	protected void parseArguments(ArgumentsMap arguments)
 			throws IllegalArgumentException {
 		String depthArgument = (String)arguments.getArgumentValue("newDepth");
-		dissolve = depthArgument.equals("NONE");
+		dissolve = depthArgument.equals("none");
 		if (!dissolve) newDepth = Integer.parseInt(depthArgument);
+		String shiftArgument = (String)arguments.getArgumentValue("shift");
+		shift = shiftArgument.equals("true");
+		editingConcatDocument = editingConcatDocument();
 	}
 
 }
