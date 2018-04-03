95ef437f32cc89eb9eb458db5a85deffa7659932
stesk <ssp@nota.nu>
2018-04-03 12:12:16 +0200

    Implement Docx import

---
+++ src/dk/nota/epub/actions/TestImportDocxAction.java
@@ -0,0 +1,42 @@
+package dk.nota.epub.actions;
+
+import java.net.URL;
+import java.util.LinkedList;
+
+import dk.nota.epub.EpubException;
+import dk.nota.epub.conversion.DocxToEpubWorker;
+import dk.nota.oxygen.EditorAccess;
+import dk.nota.oxygen.ResultsListener;
+import dk.nota.oxygen.ResultsView;
+import net.sf.saxon.s9api.XdmNode;
+
+public class TestImportDocxAction extends EpubAction {
+
+	public TestImportDocxAction() {
+		super("Docx", false);
+	}
+
+	@Override
+	public void actionPerformed(EditorAccess editorAccess,
+			LinkedList<URL> affectedEditorUrls) {
+		java.io.File[] sourceFiles = editorAccess.getPluginWorkspace()
+				.chooseFiles(null, "Import", new String[] {"docx", "kat"},
+				"Word documents, cat lists");
+		if (sourceFiles == null) return;
+		XdmNode opfDocument;
+		try {
+			opfDocument = epubAccess.getContentAccess().getOpfDocument();
+		} catch (EpubException e) {
+			editorAccess.showErrorMessage("Unable to get OPF document", e);
+			return;
+		}
+		DocxToEpubWorker docxToEpubWorker = new DocxToEpubWorker(
+				epubAccess, opfDocument, new ResultsListener(new ResultsView(
+				epubAccess.getPid() + " - Import Docx")), sourceFiles,
+				affectedEditorUrls);
+		docxToEpubWorker.execute();
+	}
+	
+	
+
+}
